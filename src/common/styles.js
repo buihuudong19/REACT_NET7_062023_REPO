a49e036b473cf30b11e3b0f77154af9273cad11c
Bui Huu Dong <dongbh@gmail.com>
2023-09-29 21:21:01 +0700

    the first contents

---
+++ src/common/styles.js
@@ -0,0 +1,17 @@
+const styleCommon = {
+    sidebarHeader:{
+        height:'65px',
+        marginTop: '-1px',
+        pading: '0 30px'
+    },
+     height410px:"410px",
+     width45px:"45px",
+     height45px:"45px",
+     height65px:"65px",
+     maxWidth700px:"700px",
+     marginTopNagative1px: "-1px",
+     paddingTopBot30px:"30px",
+}
+
+export default styleCommon;
+
